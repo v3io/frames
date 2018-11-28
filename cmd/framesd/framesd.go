65e5d54226844cd37f5e4c672a9da5ac16caa326
Miki Tebeka <miki.tebeka@gmail.com>
2018-11-28 13:13:34 +0200

    bulid process (#113)
    * docker tag & version
    * build and push docker (still no creds)
    * build binaries

--- cmd/framesd/framesd.go
+++ cmd/framesd/framesd.go
@@ -25,6 +25,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"path"
 	"time"
 
 	"github.com/ghodss/yaml"
@@ -34,19 +36,31 @@ import (
 	"github.com/v3io/frames/http"
 )
 
+var (
+	// Version is framesd version (populated by the build process)
+	Version = "unknown"
+)
+
 func main() {
 	var config struct {
-		file     string
-		httpAddr string
-		grpcAddr string
+		file        string
+		httpAddr    string
+		grpcAddr    string
+		showVersion bool
 	}
 
 	flag.StringVar(&config.file, "config", "", "path to configuration file (YAML)")
 	flag.StringVar(&config.httpAddr, "httpAddr", ":8080", "address to listen on HTTP")
 	flag.StringVar(&config.grpcAddr, "grpcAddr", ":8081", "address to listen on gRPC")
+	flag.BoolVar(&config.showVersion, "version", false, "show version and exit")
 	flag.Parse()
 
 	log.SetFlags(0) // Show only messages
+
+	if config.showVersion {
+		fmt.Printf("%s version %s\n", path.Base(os.Args[0]), Version)
+		return
+	}
 
 	if config.file == "" {
 		log.Fatal("error: no config file given")
