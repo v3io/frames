44e961c9451959aa6df32627ab36c5da32290679
Dina Nimrodi <dinan@iguazio.com>
2020-06-09 14:25:33 +0300

    print version

--- cmd/framesd/framesd.go
+++ cmd/framesd/framesd.go
@@ -48,21 +48,16 @@ func main() {
 		file        string
 		httpAddr    string
 		grpcAddr    string
-		showVersion bool
 	}
 
 	flag.StringVar(&config.file, "config", "", "path to configuration file (YAML)")
 	flag.StringVar(&config.httpAddr, "httpAddr", ":8080", "address to listen on HTTP")
 	flag.StringVar(&config.grpcAddr, "grpcAddr", ":8081", "address to listen on gRPC")
-	flag.BoolVar(&config.showVersion, "version", false, "show version and exit")
 	flag.Parse()
 
 	log.SetFlags(0) // Show only messages
 
-	if config.showVersion {
-		fmt.Printf("%s version %s\n", path.Base(os.Args[0]), Version)
-		return
-	}
+	fmt.Printf("%s version %s\n", path.Base(os.Args[0]), Version)
 
 	if config.file == "" {
 		log.Fatal("error: no config file given")
