b4398242649f31ce3b67db0cd348ccfefc0caff0
Tal Neiman <Taln@iguazio.com>
2020-06-18 11:54:15 +0300

    1e6 -> time.Milliseconds

--- backends/utils/history_server.go
+++ backends/utils/history_server.go
@@ -607,11 +607,12 @@ func (f historyFilter) filter(entry HistoryEntry) bool {
 		return false
 	}
 
-	if f.MinDuration > entry.ActionDuration.Nanoseconds()/1e6 {
+	durationTimeInMillis := entry.ActionDuration.Nanoseconds() / int64(time.Millisecond)
+	if f.MinDuration > durationTimeInMillis {
 		return false
 	}
 
-	if f.MaxDuration != 0 && f.MaxDuration < entry.ActionDuration.Nanoseconds()/1e6 {
+	if f.MaxDuration != 0 && f.MaxDuration < durationTimeInMillis {
 		return false
 	}
 
