v3io/frames

commit 38c4b8962172556cfdbdcf8607d72ea3972859ee

IG-16104 (#480)
* validate attribute names

* validate column name length

backends/kv/writer.go
@@ -23,6 +23,7 @@ package kv
 import (
 	"fmt"
 	"net/http"
+	"regexp"
 	"strings"
 	"time"
 
@@ -53,6 +54,12 @@ const (
 	falseConditionOuterErrorCode           = "16777244"
 	falseConditionInnerErrorCode           = "16777245"
 	createNewItemOnlyExistingItemErrorCode = "369098809"
+
+	maximumAttributeNameLength = 256
+)
+
+var (
+	validColumnNamePattern = regexp.MustCompile("^[a-zA-Z_][a-zA-Z0-9_]*$")
 )
 
 // Write supports writing to the backend
@@ -161,6 +168,21 @@ func validateFrameInput(frame frames.Frame, request *frames.WriteRequest) error 
 	for columnNumber, name := range names {
 		if len(name) == 0 {
 			return fmt.Errorf("column number %d has an empty name", columnNumber)
+		}
+		if !validColumnNamePattern.MatchString(name) {
+			return fmt.Errorf("column '%v' has an invalid name", name)
+		}
+		if len(name) > maximumAttributeNameLength {
+			return fmt.Errorf("column '%v' exceeding maximum allowed attribute name of %v", name, maximumAttributeNameLength)
+		}
+	}
+	for _, index := range frame.Indices() {
+		name := index.Name()
+		if !validColumnNamePattern.MatchString(name) {
+			return fmt.Errorf("index '%v' has an invalid name", name)
+		}
+		if len(name) > maximumAttributeNameLength {
+			return fmt.Errorf("column '%v' exceeding maximum allowed attribute name of %v", name, maximumAttributeNameLength)
 		}
 	}
 	if len(request.PartitionKeys) > 0 {

backends/kv/writer_test.go
@@ -100,45 +100,72 @@ func (suite *WriterTestSuite) TestGenExpr() {
 
 func (suite *WriterTestSuite) TestValidateFrameInput() {
 	column, err := frames.NewLabelColumn("col1", 5, 1)
-	suite.NoError(err)
+	suite.Require().NoError(err)
 	columns := []frames.Column{column}
 	frame, err := frames.NewFrame(columns, nil, nil)
-	if err != nil {
-		suite.NoError(err)
-	}
+	suite.Require().NoError(err)
 	writeRequest := frames.WriteRequest{}
 	err = validateFrameInput(frame, &writeRequest)
-	suite.NoError(err)
+	suite.Require().NoError(err)
 }
 
 func (suite *WriterTestSuite) TestValidateFrameInputEmptyColumnName() {
 	column, err := frames.NewLabelColumn("", 5, 1)
-	suite.NoError(err)
+	suite.Require().NoError(err)
 	columns := []frames.Column{column}
 	frame, err := frames.NewFrame(columns, nil, nil)
-	if err != nil {
-		suite.NoError(err)
-	}
+	suite.Require().NoError(err)
 	writeRequest := frames.WriteRequest{}
 	err = validateFrameInput(frame, &writeRequest)
-	suite.Error(err)
-	suite.Equal("column number 0 has an empty name", err.Error())
+	suite.Require().Error(err)
+	suite.Require().Equal("column number 0 has an empty name", err.Error())
 }
 
 func (suite *WriterTestSuite) TestValidateFrameInputRepeatingShardingKey() {
 	column, err := frames.NewLabelColumn("col1", 5, 1)
-	suite.NoError(err)
+	suite.Require().NoError(err)
 	column2, err := frames.NewLabelColumn("col2", 5, 1)
-	suite.NoError(err)
+	suite.Require().NoError(err)
 	columns := []frames.Column{column, column2}
 	frame, err := frames.NewFrame(columns, nil, nil)
-	if err != nil {
-		suite.NoError(err)
-	}
+
+	suite.Require().NoError(err)
 	writeRequest := frames.WriteRequest{PartitionKeys: []string{"col1", "col2", "col1"}}
 	err = validateFrameInput(frame, &writeRequest)
-	suite.Error(err)
-	suite.Equal("column 'col1' appears more than once as a partition key", err.Error())
+	suite.Require().Error(err)
+	suite.Require().Equal("column 'col1' appears more than once as a partition key", err.Error())
+}
+
+func (suite *WriterTestSuite) TestValidateFrameInputInvalidColumnName() {
+	column, err := frames.NewLabelColumn("****T-dawg!", 5, 1)
+	suite.Require().NoError(err)
+	columns := []frames.Column{column}
+	frame, err := frames.NewFrame(columns, nil, nil)
+	suite.Require().NoError(err)
+
+	writeRequest := frames.WriteRequest{}
+	err = validateFrameInput(frame, &writeRequest)
+	suite.Require().Error(err)
+	suite.Require().Equal("column '****T-dawg!' has an invalid name", err.Error())
+}
+
+func (suite *WriterTestSuite) TestValidateFrameInputColumnNameTooLong() {
+	columnNameRune := make([]rune, maximumAttributeNameLength+1)
+	for i := range columnNameRune {
+		columnNameRune[i] = 'a'
+	}
+
+	columnName := string(columnNameRune)
+	column, err := frames.NewLabelColumn(columnName, 5, 1)
+	suite.Require().NoError(err)
+	columns := []frames.Column{column}
+	frame, err := frames.NewFrame(columns, nil, nil)
+	suite.Require().NoError(err)
+
+	writeRequest := frames.WriteRequest{}
+	err = validateFrameInput(frame, &writeRequest)
+	suite.Require().Error(err)
+	suite.Require().Equal(fmt.Sprintf("column '%v' exceeding maximum allowed attribute name of %v", columnName, maximumAttributeNameLength), err.Error())
 }
 
 func TestWriterTestSuite(t *testing.T) {

commit 7160f81cdace52bbcc8af49610b016ecc540cffe

IG-16201: Fix empty index support.
Broken by #480 (fix for IG-16104).

backends/kv/writer.go
@@ -191,7 +191,7 @@ func validateFrameInput(frame frames.Frame, request *frames.WriteRequest) error 
 	}
 	for _, index := range frame.Indices() {
 		name := index.Name()
-		if !validColumnNamePattern.MatchString(name) {
+		if name != "" && !validColumnNamePattern.MatchString(name) {
 			return fmt.Errorf("index '%v' has an invalid name", name)
 		}
 		if len(name) > maximumAttributeNameLength {

backends/kv/writer_test.go
@@ -117,6 +117,20 @@ func (suite *WriterTestSuite) TestValidateFrameInputEmptyColumnName() {
 	suite.Require().Equal("column number 0 has an empty name", err.Error())
 }
 
+func (suite *WriterTestSuite) TestValidateFrameInputEmptyIndexName() {
+	column, err := frames.NewLabelColumn("col1", 5, 1)
+	suite.Require().NoError(err)
+	columns := []frames.Column{column}
+	index, err := frames.NewLabelColumn("", 5, 1)
+	suite.Require().NoError(err)
+	indices := []frames.Column{index}
+	frame, err := frames.NewFrame(columns, indices, nil)
+	suite.Require().NoError(err)
+	writeRequest := frames.WriteRequest{}
+	err = validateFrameInput(frame, &writeRequest)
+	suite.Require().NoError(err)
+}
+
 func (suite *WriterTestSuite) TestValidateFrameInputRepeatingShardingKey() {
 	column, err := frames.NewLabelColumn("col1", 5, 1)
 	suite.Require().NoError(err)